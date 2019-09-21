9ae2c598c8ea33784c60d5472fc80380d63f83c3
hoshimado <xingyanhuan@yahoo.co.jp>
2019-09-21 11:07:05 +0900

    ３章２節サンプルコード-3refactoring版

--- src/routes/api_v1.js
+++ src/routes/api_v1.js
@@ -9,6 +9,7 @@ var router = express.Router();
 var Factory4Hook = require('../factory4hook.js').Factory4Hook;
 
 var itemsSingleton = new Factory4Hook(require('../api_v1/crud_items'));
+// var items = require('../api_v1/items');
 
 if( process.env.NODE_ENV == 'test' ){
 	// routerの場合は、module.exports がそのまま
@@ -17,6 +18,33 @@ if( process.env.NODE_ENV == 'test' ){
 	router['itemsSingleton'] = itemsSingleton;
 }
 
+router.use('/', function (req, res, next) {
+	var headers = req.headers;
+
+	// 共通処理はここで実施
+	res.header({
+        "Access-Control-Allow-Origin" : "*", // JSONはクロスドメインがデフォルトNG。
+		"Pragma" : "no-cacha", 
+		"Cache-Control" : "no-cache",
+		"Content-Type" : "application/json; charset=utf-8"
+	});
+
+	// 次の定義ルーターへ処理を継続する。
+	next();
+});
+
+var _sendResponseAferPromise = function (targetPromise, res) {
+	return targetPromise.then(function (result) {
+		res.status(result.status).send(result.jsonData);
+		res.end();
+	}).catch((err)=>{
+		res.status(500).send(err);
+		res.end();
+	});
+};
+
+// `/api/v1` continuing...
+// router.get('/users/USER-NAME/items', function (req, res) {
 router.get('/users/:userId/items', function (req, res) {
 	var enumerateItemsByUserName 
     = itemsSingleton.getInstance().enumerateItemsByUserName;
@@ -25,21 +53,7 @@ router.get('/users/:userId/items', function (req, res) {
 	var query = req.query;
 	var promise = enumerateItemsByUserName( userName, query );
 
-	var headers = req.headers;
-	res.header({
-        "Access-Control-Allow-Origin" : "*", // JSONはクロスドメインがデフォルトNG。
-		"Pragma" : "no-cacha", 
-		"Cache-Control" : "no-cache",
-		"Content-Type" : "application/json; charset=utf-8"
-	});
-
-    promise.then(function (result) {
-		res.status(result.status).send(result.jsonData);
-		res.end();
-	}).catch((err)=>{
-		res.status(500).send(err);
-		res.end();
-	});
+	_sendResponseAferPromise(promise, res);
 });
 
 module.exports = router;
