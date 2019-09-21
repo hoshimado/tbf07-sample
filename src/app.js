420027472c87a10334d4496fa55a98f51fb71e45
hoshimado <xingyanhuan@yahoo.co.jp>
2019-09-21 10:51:22 +0900

    ３章２節サンプルコード-2.Green版

--- src/app.js
+++ src/app.js
@@ -18,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1', require('./routes/api_v1'));
 
 module.exports = app;
